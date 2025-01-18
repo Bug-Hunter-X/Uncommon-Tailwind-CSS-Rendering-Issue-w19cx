369a905ddc8fec8b33bdf4d059f09b124dce5a22
BugHunter-X <askzenai@gmail.com>
2025-01-18 06:54:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+// ... some Tailwind CSS classes
+<div class="bg-gray-100 p-4 relative">
+  <p class="text-xl font-bold absolute top-0 left-0">Some text here</p>
+  // fix the issue with absolute positioning
+</div>
+```
